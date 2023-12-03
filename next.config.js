3560eeefaf719e1367ca080c053a580b527f4e1b
Kanak <kkupgrader.fs@gmail.com>
2023-12-03 23:56:29 +0530

    initial seetup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
+    reactStrictMode: true,
+    swcMinify: true,
+    crossOrigin: 'anonymous',
+    images: {
+    domains: ['res.cloudinary.com',"global-uploads.webflow.com" ],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: "**",
+    //   },
+    // ],
+},
+
+});
+
+
+module.exports = nextConfig;
