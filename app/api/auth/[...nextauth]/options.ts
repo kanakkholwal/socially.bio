e7d35c4f9dadcc54aac4e4a8113f2c763a2092cf
Kanak <kkupgrader.fs@gmail.com>
2024-02-21 23:32:32 +0530

    Refactor authentication options and enable session provider in layout

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,35 +1,17 @@
 import { NextAuthOptions } from "next-auth";
 // Define types for environment variables
 interface AuthEnv {
-    GOOGLE_ID: string;
-    GOOGLE_SECRET: string;
+
     NEXT_AUTH_SECRET: string;
     NEXTAUTH_URL: string;
-    FACEBOOK_ID: string;
-    FACEBOOK_SECRET: string;
+
 }
 
-// Define types for user object
-interface User {
-    _id: string;
-    id?: string;
-    name: string;
-    email: string;
-    username: string;
-    account_type?: string;
-    profilePicture: string;
-    role?: string;
-    verified?: boolean;
-    provider: string;
-}
 // Read environment variables
 const env: AuthEnv = {
-    GOOGLE_ID: process.env.GOOGLE_ID || "",
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET || "",
+
     NEXT_AUTH_SECRET: process.env.NEXT_AUTH_SECRET || "",
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
-    FACEBOOK_ID: process.env.FACEBOOK_ID || "",
-    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || "",
 
 };
 
