a8c6a462a3769b80f35e0fe3fc64365da409e30a
Kanak <kkupgrader.fs@gmail.com>
2024-02-21 23:26:12 +0530

    some changes#2

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,9 @@ import Wrapper from "app/test/button";
 import { getServerSession } from "next-auth/next";
 export default async function Page() {
     const session = await getServerSession(authOptions);
-    console.log("session", session)
+    console.log("session", session);
+
+    console.log("NEXTAUTH_URL", process.env.NEXTAUTH_URL);
 
     return (<div className='flex justify-center items-center min-h-screen w-full'>
         <pre>{JSON.stringify(session, null, 2)}</pre>
